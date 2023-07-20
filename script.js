448db32996f332df21c15812ff16536de198b0f8
Patricio Mena <luispatricio.mh@gmail.com>
2023-07-20 11:43:01 -0600

    Added mobile navigation, sticky nav and set current year on footer

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+///////////////////////////////////////////////////////////
 // Scroll Animations
 const scrollObserver = new IntersectionObserver(
   (entries) => {
@@ -15,3 +16,43 @@ const scrollObserver = new IntersectionObserver(
 const hiddenSection = document.querySelectorAll('.anim');
 // Telling the observer what to observe
 hiddenSection.forEach((el) => scrollObserver.observe(el));
+
+///////////////////////////////////////////////////////////
+// Mobile Navigation
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
+
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+/////////////////////////////////////////////////////////// Sticky Navigation
+const sectionHeroEl = document.querySelector('.section-hero');
+
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add('sticky');
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px'
+  }
+);
+
+obs.observe(sectionHeroEl);
